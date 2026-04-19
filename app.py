12e90079fdfb0da53bc9a70e95032b020bc935fe
Kevin <kevinsimadjeu@gmail.com>
2026-04-19 11:56:47 +0100

    fix port 8080

--- app.py
+++ app.py
@@ -310,5 +310,5 @@ if __name__ == "__main__":
     
    import os
    
-   port = int(os.environ.get("PORT", 5000))
+   port = int(os.environ.get("PORT", 8080))
    app.run(debug=False, host="0.0.0.0", port=port)
